92101d4ae1ba35eebc3eef1790fac4820ddb4453
wubo <wb31202239@163.com>
2025-01-15 11:46:08 +0800

    fix:优化useClient 支持绑定参数配置

--- src/hooks/useClient.tsx
+++ src/hooks/useClient.tsx
@@ -1,5 +1,5 @@
 import { Draft, produce } from "immer";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useLayoutEffect, useMemo, useState } from "react";
 import { createHash } from "store-provider-helper";
 import type { AnyLike, FunctionLike } from "ts-utils-helper";
 
@@ -13,90 +13,134 @@ type UseClientOutputType<T, TArgs extends AnyLike[]> = {
 
 type ClientCache = {
   cachePromise: Promise<AnyLike>;
-  date: Date; // 缓存时间
+  timestamp: number; // 缓存时间  时间戳
   cacheResponse: AnyLike; // 缓存数据
 };
-
+type ClientOptions = {
+  maxAge?: number; // 缓存最大有效时间
+};
 const clientHashMap = new Map<string, ClientCache>();
 
-export const useClient = <
-  TClient extends FunctionLike,
-  TReturn = ReturnType<TClient>,
-  TValue = TReturn extends Promise<infer TValue> ? TValue : TReturn,
->(
-  client: TClient,
-  ...payload: Parameters<TClient>
-): UseClientOutputType<TValue, Parameters<TClient>> => {
-  const clientKey = `${createHash(client.toString())}|${createHash(payload)}`;
-  const trigger = useCallback(
-    (...payload: Parameters<TClient>) => {
-      return Promise.resolve(Reflect.apply(client, client, payload))
-        .then((data) => {
-          clientHashMap.set(clientKey, {
-            cachePromise: Promise.resolve(null),
-            date: new Date(),
-            cacheResponse: data,
+export const createUseClient = (options: ClientOptions) => {
+  return <
+    TClient extends FunctionLike,
+    TReturn = ReturnType<TClient>,
+    TValue = TReturn extends Promise<infer TValue> ? TValue : TReturn,
+  >(
+    client: TClient,
+    ...payload: Parameters<TClient>
+  ): UseClientOutputType<TValue, Parameters<TClient>> => {
+    const [isMounted, setIsMounted] = useState(false);
+    const [outPut, setOutPut] = useState<
+      UseClientOutputType<TValue, Parameters<TClient>>
+    >({
+      isLoaded: false,
+      data: undefined as TValue,
+      payload: payload,
+      get trigger() {
+        return trigger;
+      },
+    });
+    const clientKey = useMemo(
+      () => `${createHash(client.toString())}|${createHash(payload)}`,
+      [client, payload],
+    );
+    const trigger = useCallback(
+      (...payload: Parameters<TClient>) => {
+        return Promise.resolve(Reflect.apply(client, client, payload))
+          .then((data) => {
+            clientHashMap.set(clientKey, {
+              cachePromise: Promise.resolve(null),
+              timestamp: Date.now(),
+              cacheResponse: data,
+            });
+            if (isMounted) {
+              setOutPut((state) =>
+                produce(state, (draft) => {
+                  draft.error = undefined;
+                  draft.isLoaded = true;
+                  draft.data = data as Draft<TValue>;
+                  draft.payload = payload as Draft<Parameters<TClient>>;
+                }),
+              );
+            }
+            return data;
+          })
+          .catch((error) => {
+            clientHashMap.set(clientKey, {
+              cachePromise: Promise.resolve(null),
+              timestamp: Date.now(),
+              cacheResponse: error,
+            });
+            if (isMounted) {
+              setOutPut((state) =>
+                produce(state, (draft) => {
+                  draft.error = error;
+                  draft.isLoaded = true;
+                  draft.data = undefined as Draft<TValue>;
+                  draft.payload = payload as Draft<Parameters<TClient>>;
+                }),
+              );
+            }
           });
-          setOutPut((state) =>
-            produce(state, (draft) => {
-              draft.error = undefined;
-              draft.isLoaded = true;
-              draft.data = data as Draft<TValue>;
-              draft.payload = payload as Draft<Parameters<TClient>>;
-            }),
-          );
-          return data;
-        })
-        .catch((error) => {
-          clientHashMap.set(clientKey, {
-            cachePromise: Promise.resolve(null),
-            date: new Date(),
-            cacheResponse: error,
-          });
-          setOutPut((state) =>
-            produce(state, (draft) => {
-              draft.error = error;
-              draft.isLoaded = true;
-              draft.data = undefined as Draft<TValue>;
-              draft.payload = payload as Draft<Parameters<TClient>>;
-            }),
-          );
-        });
-    },
-    [client, clientKey],
-  );
-  const [outPut, setOutPut] = useState<
-    UseClientOutputType<TValue, Parameters<TClient>>
-  >({
-    isLoaded: false,
-    data: undefined as TValue,
-    payload: payload,
-    trigger,
-  });
+      },
+      [client, clientKey, isMounted],
+    );
 
-  useEffect(() => {
     if (!clientHashMap.has(clientKey)) {
       const promise = trigger(...outPut.payload);
       clientHashMap.set(clientKey, {
         cachePromise: promise,
-        date: new Date(),
+        timestamp: Date.now(),
         cacheResponse: undefined,
       });
     }
-  }, [clientKey, outPut.payload, trigger]);
-
-  return useMemo(() => {
-    const cache = clientHashMap.get(clientKey);
-    if (cache && cache.cacheResponse) {
-      return {
-        isLoaded: true,
-        data: outPut?.data ?? cache.cacheResponse,
-        payload: outPut.payload,
-        error: outPut?.error,
-        trigger,
+    useLayoutEffect(() => {
+      setIsMounted(true);
+      return () => {
+        setIsMounted(false);
       };
-    } else {
-      throw Promise.resolve(null);
-    }
-  }, [clientKey, outPut, trigger]);
+    }, []);
+
+    useLayoutEffect(() => {
+      return () => {
+        const cacheTimestamp = clientHashMap.get(clientKey)?.timestamp;
+        /**
+         * @description 当缓存过期 或者 未设置缓存事件时候 删除缓存
+         */
+        if (
+          isMounted &&
+          cacheTimestamp &&
+          (options.maxAge === undefined ||
+            Date.now() - cacheTimestamp > options.maxAge)
+        ) {
+          clientHashMap.delete(clientKey);
+        }
+      };
+    }, [clientKey, isMounted]);
+
+    return useMemo(() => {
+      const cache = clientHashMap.get(clientKey);
+      if (cache && cache.cacheResponse) {
+        return {
+          isLoaded: true,
+          data: outPut?.data ?? cache.cacheResponse,
+          payload: outPut.payload,
+          error: outPut?.error,
+          trigger,
+        };
+      } else if (outPut.isLoaded) {
+        return outPut;
+      } else {
+        throw Promise.resolve(null);
+      }
+    }, [clientKey, outPut, trigger]);
+  };
 };
+
+export const useClient = Object.assign(createUseClient({}), {
+  bind: (options: ClientOptions) => {
+    return createUseClient(options);
+  },
+});
+export default useClient;
